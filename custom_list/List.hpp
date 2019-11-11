299ee401eac8a69524917ede3131e4690569d5d0
Tomasz Tomczak <gh@tomasztomczak.pl>
2019-11-11 22:18:38 +0100

    list push back implementation

--- custom_list/List.hpp
+++ custom_list/List.hpp
@@ -9,11 +9,11 @@ class List
         public:
         Node(const U& value)
         {
-
+            object = value;
         }
        
-        Node<U>* previous;
-        Node<U>* next;
+        Node<U>* previous = nullptr;
+        Node<U>* next = nullptr;
         U object
 
     };
@@ -21,6 +21,19 @@ class List
     public:
     void push_back(const T& value)
     {
+        Node<T>* new_element = new Node<T>(value);
+        if(size == 0)
+        {
+            begin = new_element;
+            end = new_element;
+        }
+        else
+        {
+            end->next = new_element;
+            new_element-> previous = end;
+            end = new_element;
+        }
+        size ++;
         
     }
     void push_back(T&& value)
