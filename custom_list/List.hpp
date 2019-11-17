3cf0d9d876e42aee321ad1e0f46a9cc4038eddd5
Tomasz Tomczak <gh@tomasztomczak.pl>
2019-11-17 11:52:10 +0100

    few methods added

--- custom_list/List.hpp
+++ custom_list/List.hpp
@@ -41,7 +41,8 @@ class List
     iteratorBase(const iteratorBase<WasConst> &rhs) : ptr_(rhs.ptr_){/*std::cout << "conversion to const" << std::endl;*/};
     iteratorBase() {}
 
-
+    bool operator==(const self_type &rhs) { return ptr_ == rhs.ptr_; }
+    bool operator!=(const self_type &rhs) { return ptr_ != rhs.ptr_; }
     self_type operator++()
     {
         std::cout<< "++"<<std::endl;
@@ -49,15 +50,27 @@ class List
       ptr_ = ptr_->next;
       return i;
     }
-    self_type operator++(int junk)
+    self_type operator++(int junk) //post increment
     {
                 std::cout<< "++ with junk"<<std::endl;
-      ptr_++;
+      ptr_ = ptr_->next;
+      return *this;
+    }
+    self_type operator--()
+    {
+      std::cout<< "--"<<std::endl;
+      self_type i = *this;
+      ptr_ = ptr_->previous;
+      return i;
+    }
+     self_type operator--(int junk) //post decrement
+    {
+      ptr_ = ptr_->previous;
       return *this;
     }
     self_type operator=(const self_type &other)
     {
-        std::cout<<"="<<std::endl;
+      std::cout<<"="<<std::endl;
       ptr_ = other.ptr_;
       return *this;
     }
@@ -71,8 +84,7 @@ class List
       std::cout<<"*"<<std::endl;
       return ptr_->object;
     }
-    bool operator==(const self_type &rhs) { return ptr_ == rhs.ptr_; }
-    bool operator!=(const self_type &rhs) { return ptr_ != rhs.ptr_; }
+
     // TODO: implement -- operator
 
     friend iteratorBase<true>; //ugh... I believe somewhere exists better way to handle this
@@ -83,6 +95,24 @@ class List
   typedef iteratorBase<true> const_iterator;
   typedef iteratorBase<false> iterator;
 
+    T& front()
+    {
+        return m_head->object;
+    }
+    const T& front() const
+    {
+        return m_head->object;
+    }
+
+    T& back()
+    {
+        return m_tail->object;
+    }
+    const T& back() const
+    {
+        return m_tail->object;
+    }
+
     iterator begin()
     {
         return iterator(m_head);
@@ -91,6 +121,37 @@ class List
     {
         return iterator(m_tail->next);
     }
+    bool empty() const noexcept
+    {
+       return size==0;
+    }
+    size_t size() const noexcept
+    {
+        return size;
+    }
+    size_t max_size()
+    {
+        return 0; //TODO: implement
+    }
+    void clear() noexcept
+    {
+
+    }
+    iterator insert(const_iterator pos, const T& value);
+    iterator insert(const_iterator pos, T&& value);
+    iterator insert(const_iterator pos, size_t count, const T& value);
+    template<class InputIterator>
+    iterator insert(const_iterator pos, InputIterator first, InputIterator last);
+    iterator insert(const_iterator pos, std::initializer_list<T> ilist);
+
+    template<class... Args>
+    iterator emplace(const_iterator pos, Args&&... args);
+    iterator erase(const_iterator pos);
+    iterator erase(const_iterator first, const_iterator last);
+
+
+
+
     void push_back(const T& value)
     {
         Node<T>* new_element = new Node<T>(value);
@@ -113,8 +174,22 @@ class List
 
     }*/
 
+    template<class... Args>
+    T& emplace_back(Args&&... args);
+    void pop_back();
+    void push_front(const T& value);
+    void push_front(T&& value);
+
+    template<class... Args>
+    T& emplace_front(Args&&... args);
+    void pop_front();
+    void resize(size_t count);
+    void resize(size_t count, const T& value);
+    void swap(List& other);
+    
+
     private:
-    int size = 0;
+    size_t size = 0;
     Node<T>* m_head = nullptr;
     Node<T>* m_tail = nullptr;
 };
